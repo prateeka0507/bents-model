cd270101c1e7eb705df371c48c50c282c278be2e
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-09-28 03:44:51 +0000

    Update Section1.jsx

--- frontend/bents_woodworking/src/Section1.jsx
+++ frontend/bents_woodworking/src/Section1.jsx
@@ -65,10 +65,12 @@ export default function Section1() {
     e.preventDefault();
     try {
       // Send form data to the backend API
-      const response =await axios.post('https://bents-model-backend.vercel.app/contact', formData, {
-  withCredentials: true
-});
-      alert(response.data.message);
+ const response = await axios.post('https://bents-model-backend.vercel.app/contact', formData, {
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});   alert(response.data.message);
       setFormData({ name: '', email: '', subject: '', message: '' });
     } catch (error) {
       console.log(error);
