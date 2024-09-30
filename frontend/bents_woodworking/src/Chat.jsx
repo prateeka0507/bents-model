62c46a8e79c9d1c43923ad23d21b1174130dfbfa
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-09-30 16:15:01 +0530

    Update Chat.jsx

--- frontend/bents_woodworking/src/Chat.jsx
+++ frontend/bents_woodworking/src/Chat.jsx
@@ -111,7 +111,7 @@ export default function Chat() {
     
     try {
       // Simulating a longer load time (e.g., 3 seconds)
-       await new Promise(resolve => setTimeout(resolve, 5000));
+       await new Promise(resolve => setTimeout(resolve, 10000));
        const response = await axios.post('https://bents-model-backend.vercel.app/chat', {
         message: query,
         selected_index: selectedIndex,
