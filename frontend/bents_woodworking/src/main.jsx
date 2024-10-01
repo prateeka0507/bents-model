65d9307562a4f8ab8248326196426c220ccc0c80
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-10-01 10:38:14 +0530

    Update main.jsx

--- frontend/bents_woodworking/src/main.jsx
+++ frontend/bents_woodworking/src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const Root = () => (
   <StrictMode>
+    <Helmet>
+      <title>Bent's Woodworking Assistant</title>
+      <link rel="icon" type="image/png" href="/path-to-your-favicon.png" />
+    </Helmet>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
+
+createRoot(document.getElementById('root')).render(<Root />)
