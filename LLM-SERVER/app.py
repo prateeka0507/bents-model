e2f328bb0b86cfc36f43594d92e8d2179130805c
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-10-01 16:09:29 +0530

    Update app.py

--- LLM-SERVER/app.py
+++ LLM-SERVER/app.py
@@ -380,13 +380,13 @@ def chat():
         logging.debug(f"Final related products: {related_products}")
 
         response_data = {
-            'response': processed_answer,
-            'initial_answer': initial_answer,
-            'related_products': related_products,
-            'url': url,
-            'context': context,
-            'video_links': video_dict,
-            'video_title': video_title
+    'response': processed_answer,
+    'initial_answer': initial_answer,
+    'related_products': related_products,
+    'url': url,
+    'context': context,
+    'video_links': video_dict,
+    'video_title': video_title
 }
 
         return jsonify(response_data)
