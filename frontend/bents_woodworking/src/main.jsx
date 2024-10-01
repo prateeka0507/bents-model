a516b386056f4525093ebc4b8976903be6cb32a9
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-10-01 10:41:39 +0530

    Update main.jsx

--- frontend/bents_woodworking/src/main.jsx
+++ frontend/bents_woodworking/src/main.jsx
@@ -1,20 +1,29 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Helmet } from 'react-helmet'
 import App from './App.jsx'
 import './index.css'
 
-const Root = () => (
-  <StrictMode>
-    <Helmet>
-      <title>Bent's Woodworking Assistant</title>
-      <link rel="icon" type="image/png" href="/bents-logo.jpg" />
-    </Helmet>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-)
+const Root = () => {
+  useEffect(() => {
+    // Change the title
+    document.title = "Bent's Woodworking Assistant"
+
+    // Change the favicon
+    const link = document.querySelector("link[rel~='icon']") || document.createElement('link')
+    link.type = 'image/png'
+    link.rel = 'icon'
+    link.href = '/bents-logo.jpg' // Update this path
+    document.getElementsByTagName('head')[0].appendChild(link)
+  }, [])
+
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+}
 
 createRoot(document.getElementById('root')).render(<Root />)
