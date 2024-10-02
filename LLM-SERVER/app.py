0da806dda9384b7ec3f746bddd3b1eaaa518392b
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-10-02 22:31:32 +0530

    Update app.py

--- LLM-SERVER/app.py
+++ LLM-SERVER/app.py
@@ -22,8 +22,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
     "https://bents-model-backend.vercel.app",
-    "https://www.bentsassistant.com",
-    "https://bentsassistant.com"
+    "https://bents-model-frontend.vercel.app"
 ]}})
 
 
