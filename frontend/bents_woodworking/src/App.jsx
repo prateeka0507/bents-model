582be89b74d95295f107de43d52677cd259ed052
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-10-02 13:16:05 +0530

    Update App.jsx

--- frontend/bents_woodworking/src/App.jsx
+++ frontend/bents_woodworking/src/App.jsx
@@ -7,7 +7,6 @@ import Chat from './Chat';
 import Shop from './Shop';
 function App() {
   const location = useLocation();
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
