8f70291851d05c0a5b84ae72c5314e0002d4d2ed
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-10-02 22:25:27 +0530

    Update index.cjs

--- backend/index.cjs
+++ backend/index.cjs
@@ -11,7 +11,7 @@ const port = 5002;
 
 
 const corsOptions = {
-  origin: "https://www.bentsassistant.com", 
+  origin: ['https://bents-model-frontend.vercel.app', '*'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
