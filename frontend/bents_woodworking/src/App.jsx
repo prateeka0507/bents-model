83c44028feae9522d62c584daad5817e1edfbff5
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-10-04 14:45:10 +0530

    Update App.jsx

--- frontend/bents_woodworking/src/App.jsx
+++ frontend/bents_woodworking/src/App.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
+import { ChatProvider } from './ChatContext';
 import Header from './Header';
 import Section1 from './Section1';
 import Footer from './Footer';
 import Chat from './Chat';
 import Shop from './Shop';
 
-export default function App() {
+function App() {
   const location = useLocation();
-
   const showFooter = location.pathname !== '/chat';
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className={`flex-grow ${location.pathname !== '/' ? 'pt-[75px]' : ''}`}>
-        <Routes>
-          <Route path="/" element={<Section1 />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/shop" element={<Shop />} />
-        </Routes>
-      </main>
-      {showFooter && <Footer />}
-    </div>
+    <ChatProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className={`flex-grow ${location.pathname !== '/' ? 'pt-[75px]' : ''}`}>
+          <Routes>
+            <Route path="/" element={<Section1 />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+        </main>
+        {showFooter && <Footer />}
+      </div>
+    </ChatProvider>
   );
 }
+
+export default App;
+
+
+
+
+
+
+
+
+
